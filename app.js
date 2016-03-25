c0e43d0713c4e6f2b12c84d0c11df502e00d550c
Boasbabs <babalolasimeon@gmail.com>
2016-03-25 13:51:54 +0530

    inital commit after setting up css and html

---
+++ app.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+	// Getting screen resolutions and positioning start button
+	var width =screen.width - 100;
+	var height = screen.height - 200;
+	var code = 0;	
+})

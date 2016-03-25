2ba8d1f15289defc6be182de8872d81ba29f3633
Boasbabs <babalolasimeon@gmail.com>
2016-03-25 16:00:22 +0530

    i have written the js parts that generate letters and random colors. but there is a bug. it is not changing position and color of the bubble

--- app.js
+++ app.js
@@ -3,4 +3,50 @@ $(document).ready(function () {
 	var width =screen.width - 100;
 	var height = screen.height - 200;
 	var code = 0;	
-})
+
+	$("#start").css(
+		{	"top": (height/2) +"px",
+			"left": (width/2) + "px" }	
+	);
+	$("#start").click( function () {
+		// after clicking button show fade and then show game
+		$(this).fadeOut('slow');
+		$('#score').show();
+		genLetter();
+	});
+	//Dealing KeyEvents and fading out matched bubble
+	$(document).keydown( function(event) {
+		var keycode = event.keyCode;
+		$('.bubb'+keycode).animate(
+				{"top":height + "px", "opacity":0 }, "slow"
+			);
+		$(".bubb"+keycode).fadeOut("slow").hide("slow", function() {
+			code+=20;
+			$("#score").html(code);
+			$(this).remove();
+		});
+	});
+	//Generating Letters from A - Z
+	function genLetter() {
+		var color = randomColor();
+		var k = Math.floor(Math.random() * (90 - 65 + 1)) + 65;
+		var ch = String.fromCharCode(k);
+		var top = Math.floor(Math.random() * height);
+		var left = Math.floor(Math.random() * width);
+		$("#container").append('<span class="bubb bubb'+ k +'" style="left: '+ left +'; top: '+ top +'; background-color:'+ color +'">'+ ch +'</span>');
+		setTimeout(genLetter, 1000);
+	}
+
+	//Generating Colors
+	function randomColor() {
+		var color = "";
+		var values = ["a", 'b', 'c', 'd', 'e', 'f', '1', '2', '3', '4', '5', '6', 
+				'7', '8', '9', '0'];
+		for (var i = 0; i < 6; i++) {
+			var no = Math.floor(Math.random() * 15);
+			color += values[no]
+			}
+			return color; 
+		}
+
+});
